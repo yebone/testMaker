7ece5f703ed314aef7ffee1f35331deaf8624822
unknown <yebonemyat@gmail.com>
2023-10-03 14:26:37 +0630

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className=" bg-slate-200 h-[100vh] flex justify-center items-center ">
+      <h1 className=" text-black text-5xl font-extrabold drop-shadow-2xl">
+        Hello! this is starter pack!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
